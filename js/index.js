441affe676c617e876b4c6d0acb5c626194391a2
m-baqir <n01307731@humbermail.ca>
2021-09-01 23:11:37 -0400

    sticky header

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
-/*Code for creating a sticky header that stays on screen as user scrolls */
-window.onscroll = function () { myFunction() };
+window.onload = function () {
+    /*Code for creating a sticky header that stays on screen as user scrolls */
+    window.onscroll = function () { myFunction() };
 
-var header = document.getElementsByTagName("header");
+    var header = document.getElementById("myheader");
 
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
+    var sticky = header.offsetTop;
+    console.log(sticky);
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
     }
-}
+
+
+};
 
 /*code for displaying projects tabs */
 function openProject(evt, project) {
@@ -19,7 +23,7 @@ function openProject(evt, project) {
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
-    }    
+    }
     document.getElementById(project).style.display = "block";
-    
+
 }
